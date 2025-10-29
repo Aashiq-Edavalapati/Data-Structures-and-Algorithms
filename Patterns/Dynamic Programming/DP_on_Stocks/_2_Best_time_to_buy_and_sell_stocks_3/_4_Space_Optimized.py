9909c8cae4c34c3452b3dbc878c4a2793bf51ffc
Aashiq-Edavalapati <aashiqedavalapati58@gmail.com>
2025-10-29 22:54:11 +0530

    Add DP/Best time to buy and sell stocks 3

---
+++ Patterns/Dynamic Programming/DP_on_Stocks/_2_Best_time_to_buy_and_sell_stocks_3/_4_Space_Optimized.py
@@ -0,0 +1,106 @@
+# Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/description/
+
+"""
+    @question:
+        You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+        Find the maximum profit you can achieve. You may complete at most two transactions.
+
+        Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+        
+    ------------------------------------------------------------------------------------------------------------
+        
+        Example 1:
+            Input: prices = [3,3,5,0,0,3,1,4]
+            Output: 6
+            Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+            Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+        
+        -------------------------------------------------------------------------------------------
+        
+        Example 2:
+            Input: prices = [1,2,3,4,5]
+            Output: 4
+            Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+            Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are engaging multiple transactions at the same time. You must sell before buying again.
+        
+        --------------------------------------------------------------------------------------------------
+            
+        Example 3:
+            Input: prices = [7,6,4,3,1]
+            Output: 0
+            Explanation: In this case, no transaction is done, i.e. max profit = 0.
+    
+    -----------------------------------------------------------------------------------
+
+        Constraints:
+            1 <= prices.length <= 105
+            0 <= prices[i] <= 105
+"""
+
+from typing import List
+
+
+def maxProfit(prices: List[int]) -> int:
+    """
+        prev => prev
+        dp[idx] => curr
+    """
+    n = len(prices)
+    # Initialize prev row
+    prev = [[0, 0] for _ in range(3)]
+
+    for idx in range(n - 1, -1, -1):
+        # Curr Row
+        curr = [[0, 0] for _ in range(3)]
+        for sold in range(1, -1, -1):
+            for canBuy in range(1, -1, -1):
+                if canBuy:
+                    buy = -prices[idx] + prev[sold][0]
+                    notBuy = prev[sold][1]
+                    profit = max(buy, notBuy)
+                else:
+                    sell = prices[idx] + prev[sold + 1][1]
+                    notSell = prev[sold][0]
+                    profit = max(sell, notSell)
+                
+                # Store res in curr row
+                curr[sold][canBuy] = profit
+        # Update curr row as prev for next row
+        prev = curr
+
+    return prev[0][1]
+
+def maxProfit2(prices: List[int]) -> int:
+    """
+        Method 2: After each buy or sell, increase the transactions count => 
+                        Even count => Buy, Odd Count => Sell
+    """
+    n = len(prices)
+    prev = [0] * 5
+    for idx in range(n - 1, -1, -1):
+        curr = [0] * 5
+        for transaction in range(3, -1, -1):
+            if transaction % 2 == 0:
+                buy = -prices[idx] + prev[transaction + 1]
+                notBuy = prev[transaction]
+                profit = max(buy, notBuy)
+            else:
+                sell = prices[idx] + prev[transaction + 1]
+                notSell = prev[transaction]
+                profit = max(sell, notSell)
+
+            curr[transaction] = profit
+        prev = curr
+
+    return prev[0]
+
+if __name__ == '__main__':
+    testCases = [
+        [3,3,5,0,0,3,1,4],  # 6
+        [1,2,3,4,5],    # 4
+        [7,6,4,3,1],    # 0
+    ]
+
+    for i, prices in enumerate(testCases):
+        print(f"TestCase {i}: i/p: prices={prices}; o/p: {maxProfit2(prices)}")
