3d03ae9d1a6ea85f655899d02bdc5968ee6c8c09
Aashiq-Edavalapati <aashiqedavalapati58@gmail.com>
2026-05-11 01:35:19 +0530

    ques(tree): lc_101: symmetric tree or not

--- Problems/Binary_Trees/LC_101_symmetric_tree/sol.py
+++ Problems/Binary_Trees/LC_101_symmetric_tree/sol.py
@@ -1,4 +1,4 @@
-# Link: https://www.geeksforgeeks.org/problems/bottom-view-of-binary-tree/1
+# Link: https://leetcode.com/problems/symmetric-tree/
 
 from importlib.util import module_from_spec, spec_from_file_location
 from pathlib import Path
@@ -25,33 +25,20 @@ TreeNode = _binary_tree_module.TreeNode
 build_tree_from_list = _binary_tree_module.build_tree_from_list
 
 
-def bottomView(root: Optional[TreeNode]) -> List[int]:
-    """
-        @intuition: 
-            Last element of every col from left to right
-    """
-    q = [(0, root)] # (x, node)
-    vals = {}
-    minCol, maxCol = 0, 0
+def isSymmetric(root: Optional[TreeNode]) -> bool:
+    if not root: return True
     
-    while q:
-        size = len(q)
-        
-        for _ in range(size):
-            col, curr = q.pop(0)
-            minCol = min(minCol, col)
-            maxCol = max(maxCol, col)
-            vals[col] = curr.val
-            
-            if curr.left: q.append((col - 1, curr.left))
-            if curr.right: q.append((col + 1, curr.right))
-    
-    return [vals[k] for k in range(minCol, maxCol +  1)]
+    def check(node1, node2):
+        if not node1 or not node2: return not node1 and not node2
+        return node1.val == node2.val and check(node1.left, node2.right) and check(node1.right, node2.left)
+
+    return check(root.left, root.right)
     
 
 if __name__ == '__main__':
 	testCases = [
 		[1, 2, 3],
+		[1,2,2,3,4,4,3],
         [1, 2, None, 3, 4, 5, 6, 7, 8],
         [1, 2, 4, 5, None, None, 6, 7, 8, 9]
 	]
@@ -59,4 +46,4 @@ if __name__ == '__main__':
 	for i, testCase in enumerate(testCases):
 		root = build_tree_from_list(testCase)
 		print(f"Testcase {i}:- i/p: {testCase}")
-		print(f"Output: {bottomView(root)}")
+		print(f"Output: {isSymmetric(root)}")
