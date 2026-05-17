2b00102ac371c1418a007d493b9a0470d66bd0eb
Aashiq-Edavalapati <aashiqedavalapati58@gmail.com>
2026-05-17 12:56:10 +0530

    ques(bst): delete from bst

--- Problems/Binary_Search_Trees/LC_450_delete_from_BST/sol.py
+++ Problems/Binary_Search_Trees/LC_450_delete_from_BST/sol.py
@@ -1,40 +1,44 @@
-# Link: https://leetcode.com/problems/insert-into-a-binary-search-tree/
+# Link: https://leetcode.com/problems/delete-node-in-a-bst/
 
 """
     @question:
-        You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
-
-        Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+        Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+        Basically, the deletion can be divided into two stages:
+            Search for a node to remove.
+            If the node is found, delete the node.
 
     --------------------------------------------------------------------------------------
     --------------------------------------------------------------------------------------
 
         Example 1:
-            Input: root = [4,2,7,1,3], val = 5
-            Output: [4,2,7,1,3,5]
-            Explanation: Another accepted tree is:
+            Input: root = [5,3,6,2,4,null,7], key = 3
+            Output: [5,4,6,2,null,null,7]
+            Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
+            One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
+            Please notice that another valid answer is [5,2,6,null,4,null,7] and it's also accepted.
 
         --------------------------------------------------------------------------------------
             
         Example 2:
-            Input: root = [40,20,60,10,30,50,70], val = 25
-            Output: [40,20,60,10,30,50,70,null,null,25]
+            Input: root = [5,3,6,2,4,null,7], key = 0
+            Output: [5,3,6,2,4,null,7]
+            Explanation: The tree does not contain a node with value = 0.
 
         --------------------------------------------------------------------------------------
             
         Example 3:
-            Input: root = [4,2,7,1,3,null,null,null,null,null,null], val = 5
-            Output: [4,2,7,1,3,5]
-
+            Input: root = [], key = 0
+            Output: []
+        
     --------------------------------------------------------------------------------------
     --------------------------------------------------------------------------------------
 
         Constraints:
-            The number of nodes in the tree will be in the range [0, 104].
-            -108 <= Node.val <= 108
-            All the values Node.val are unique.
-            -108 <= val <= 108
-            It's guaranteed that val does not exist in the original BST.
+            The number of nodes in the tree is in the range [0, 104].
+            -105 <= Node.val <= 105
+            Each node has a unique value.
+            root is a valid binary search tree.
+            -105 <= key <= 105
 """
 from importlib.util import module_from_spec, spec_from_file_location
 from pathlib import Path
@@ -76,24 +80,32 @@ BinaryTree = _binary_tree_module.BinaryTree
 TreeNode = _binary_tree_module.TreeNode
 build_tree_from_list = _binary_tree_module.build_tree_from_list
 
-def insertIntoBST(root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-    if not root: return TreeNode(val)
+def deleteNode(root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+    if not root: return
+    if root.val == key:
+        node = root.right
+        if not node: return root.left
+        n = root.left
+        if not n: return node
+        t = n
+        while n and n.right:
+            n = n.right
+        n.right = node
 
-    if root.val > val:
-        if not root.left: root.left = TreeNode(val)
-        else: insertIntoBST(root.left, val)
+        return t
+    
+    if root.val < key:
+        root.right = deleteNode(root.right, key)
     else:
-        if not root.right: root.right = TreeNode(val)
-        else: insertIntoBST(root.right, val)
-
+        root.left = deleteNode(root.left, key)
+    
     return root
 
 if __name__ == '__main__':
     testCases = [
-        [[8, 4, 12, 2, 6, 10, 14] , 11],
-        [[8, 4, 12, 2, 6, 10, 14] , 15],
-        [[8, 4, 12, 2, 6, 10, 14] , 1],
-        [[8, 4, 12, 2, 6, 10, 14] , 6]
+        [[5,3,6,2,4,None,7], 3],
+        [[5,3,6,2,4,None,7], 0],
+        [[], 0]
     ]
 
     for i, (tree, val) in enumerate(testCases):
@@ -102,6 +114,6 @@ if __name__ == '__main__':
         print(f"Testcase {i}:")
         print(f"Input: val={val}, root: \n")
         display_tree(root)
-        newRoot = insertIntoBST(root, val)
+        newRoot = deleteNode(root, val)
         print(f"Output: \n")
         display_tree(newRoot)
